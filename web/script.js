d51a859b6aa0a65fb3b248635f2e38c434ff3c24
Steven Fawcett <stevenfawcett@MacBook-Pro.local>
2022-12-04 21:06:38 +0000

    scale to size of canvas

--- web/script.js
+++ web/script.js
@@ -4,6 +4,17 @@ const context = canvas.getContext('2d');
 const img = new Image();
 img.src = './images/Nautical_compass_rose.svg';
 
-img.onload = () => {          context.drawImage(img, 0, 0);        };
+img.onload = () => {         drawImageScaled(img, context);        };
 
+function drawImageScaled(img, ctx) {
+   var canvas = ctx.canvas ;
+   var hRatio = canvas.width  / img.width    ;
+   var vRatio =  canvas.height / img.height  ;
+   var ratio  = Math.min ( hRatio, vRatio );
+   var centerShift_x = ( canvas.width - img.width*ratio ) / 2;
+   var centerShift_y = ( canvas.height - img.height*ratio ) / 2;  
+   ctx.clearRect(0,0,canvas.width, canvas.height);
+   ctx.drawImage(img, 0,0, img.width, img.height,
+                      centerShift_x,centerShift_y,img.width*ratio, img.height*ratio);  
+}
 
