c525251a2161f50da813471c88bc0251367b1b06
Steven Fawcett <steve.fawcett@capgemini.com>
2022-12-05 12:56:28 +0000

    Step 1

--- src/index.mjs
+++ src/index.mjs
@@ -2,11 +2,22 @@ import * as Files from "./modules/file.js"
 
 export function Compass() {
     var canvas = document.querySelector("canvas");
-    const context = canvas.getContext('2d');
-    const img = new Image();
-    img.src = Files.Path() + '/images/Nautical_compass_rose.svg';
+    var ctx = canvas.getContext('2d');
 
-    img.onload = () => { drawImageScaled(img, context); };
+
+    const img = new Image();
+    img.src = Files.Path() + '/images/aircraft_bezel.png';
+
+    const img2 = new Image();
+    img2.src = Files.Path() + '/images/aircraft_compass.png';
+
+    img.onload = () => {
+        drawImageScaled(img, ctx );
+    }
+
+    img2.onload = () => {
+        drawImageScaled(img2, ctx );
+    }
 
     function drawImageScaled(img, ctx) {
         var canvas = ctx.canvas;
@@ -15,7 +26,6 @@ export function Compass() {
         var ratio = Math.min(hRatio, vRatio);
         var centerShift_x = (canvas.width - img.width * ratio) / 2;
         var centerShift_y = (canvas.height - img.height * ratio) / 2;
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.drawImage(img, 0, 0, img.width, img.height,
             centerShift_x, centerShift_y, img.width * ratio, img.height * ratio);
     }
