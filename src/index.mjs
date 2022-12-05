e12939f8a68860d427cae698b20d3a578534b040
Steven Fawcett <stevenfawcett@MacBook-Pro.local>
2022-12-05 21:42:40 +0000

    getting better

--- src/index.mjs
+++ src/index.mjs
@@ -11,17 +11,19 @@ export function Compass() {
     var ang = 0; //angle
     var fps = 1000 / 25; //number of frames per sec
     img.onload = function () { //on image load do the following stuff
-        canvas.width = this.width << 1; //double the canvas width
-        canvas.height = this.height << 1; //double the canvas height
+        canvas.width = 500; //Any width
+        canvas.height = 500; //Any height
         var cache = this; //cache the local copy of image element for future reference
+        var iw = cache.width;
+        var ih = cache.height;
         setInterval(function () {
             ctx.save(); //saves the state of canvas
             ctx.clearRect(0, 0, canvas.width, canvas.height); //clear the canvas
-            ctx.translate(cache.width, cache.height); //let's translate
+            ctx.translate(canvas.width/2, canvas.height/2); //let's translate
             ctx.rotate(Math.PI / 180 * (ang += 5)); //increment the angle and rotate the image 
-            ctx.drawImage(img, -cache.width / 2, -cache.height / 2, cache.width, cache.height); //draw the image ;)
+            ctx.translate(-(canvas.width/2), -(canvas.height/2)); //let's translate
+            ctx.drawImage(img, canvas.width/2 - iw/2, canvas.height/2 - ih/2, iw, ih); //draw the image ;)
             ctx.restore(); //restore the state of canvas
         }, fps);
-    };
-
-};
+    }
+}
