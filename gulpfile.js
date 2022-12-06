0b9c3ad36b163d5d522ebfaa77aa05aa7dd56469
Steven Fawcett <stevenfawcett@MacBook-Pro.local>
2022-12-06 22:04:36 +0000

    Adding gulp build

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const {  src, dest, series } = require("gulp");
+var clean = require('gulp-clean');
+var browserify = require('browserify');
+var esmify = require('esmify');
+var source = require("vinyl-source-stream");
+
+function images( cb ) {
+    src('./src/images/*.png')
+            .pipe(dest('dist/img/'));
+    cb();
+ }
+
+ function javascripts() {
+    return browserify({
+        entries: './src/index.mjs',
+        debug: true,
+        standalone: 'instruments',
+        plugin: [ esmify ]
+      })
+      .bundle()
+      .pipe(source("instruments.js"))
+      .pipe(dest('dist/js'));
+ }
+
+ function cleanup(cb) {
+    src('dist/**', {read: false})
+        .pipe(clean());
+    cb();
+ }
+
+ exports.clean = cleanup;
+
+ exports.default = series( cleanup , images , javascripts )
